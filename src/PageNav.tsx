7b34fd1552c72408e57b195ff07e2ca6b5017c3c
David Mlnarik <mgrmlnarik@gmail.com>
2018-05-13 15:50:26 +0200

    adding tween.js and slick-carousel

--- src/PageNav.tsx
+++ src/PageNav.tsx
@@ -1,24 +1,46 @@
+import { Easing, Group, Tween } from '@tweenjs/tween.js';
 import * as React from "react";
+import { PortraitList } from './PortraitList';
 
 export class PageNavWrapper extends React.Component {
 
+    private cube = {rotation: 0};
+    private tween: Tween;
+    private animationGroup: Group;
+
+
     constructor(props: any) {
         super(props);
+        this.animationGroup = new Group();
+        this.tween = new Tween(this.cube, this.animationGroup).to({rotation: 360}, 1000).easing(Easing.Quintic.InOut);
+    }
 
+    public componentDidMount() {
+        this.tween.start();
+        setInterval(this.animationLoop, 1000/60);
     }
 
     public render() {
-    return (
-        <div>
-            <header className="page-nav">
+        const style = {
+            transform: `rotate(-${this.cube.rotation}deg)`
+        };
+
+        return (
+        <div className="page-nav">
+            <header style={style}>
                 <div className="north-nav"><a href="">Portraits</a></div>
                 <div className="west-nav"><a href="">Acting</a></div>
                 <div className="east-nav"><a href="">Developing</a></div>
                 <div className="south-nav"><a href="">Dancing</a></div>
             </header>
             <div className="page-content">
-                {this.props.children}
+                <PortraitList />
             </div>
         </div>);
+    }
+
+    private animationLoop = () => {
+        this.animationGroup.update();
+        this.setState({});
     }
 }
--- src/PortraitList.tsx
+++ src/PortraitList.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Slider from 'react-slick';
 import * as portrait1 from './portraits/a1.png';
 import * as portrait2 from './portraits/a2.png';
 import * as portrait3 from './portraits/a3.png';
@@ -8,13 +9,37 @@ import * as portrait6 from './portraits/b2.png';
 import * as portrait7 from './portraits/b3.png';
 
 export const PortraitList = () => {
-    return (<div>
-      <img className="animated fadeIn" src={portrait1} />
-      <img className="animated fadeIn" src={portrait2} />
-      <img className="animated fadeIn" src={portrait3} />
-      <img className="animated fadeIn" src={portrait4} />
-      <img className="animated fadeIn" src={portrait5} />
-      <img className="animated fadeIn" src={portrait6} />
-      <img className="animated fadeIn" src={portrait7} />
-      </div>);
+    const settings = {
+      arrows: true,
+      autoplay: true,
+      centerMode: true,
+      infinite: true,
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      speed: 500
+    }
+    return (
+      <Slider {...settings} className="slider-images">
+        <div>
+          <img className="animated fadeIn" src={portrait1} />
+        </div>
+        <div>
+          <img className="animated fadeIn" src={portrait2} />
+        </div>
+        <div>
+          <img className="animated fadeIn" src={portrait3} />
+        </div>
+        <div>
+          <img className="animated fadeIn" src={portrait4} />
+        </div>
+        <div>
+          <img className="animated fadeIn" src={portrait5} />
+        </div>
+        <div>
+          <img className="animated fadeIn" src={portrait6} />
+        </div>
+        <div>
+          <img className="animated fadeIn" src={portrait7} />
+        </div>
+      </Slider>);
   }
