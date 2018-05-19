8314518d93b6a3993529c7b2db36e6fc9c2489eb
David Mlnarik <mgrmlnarik@gmail.com>
2018-05-19 22:42:38 +0200

    size adjustment and adding link

--- src/DevelopingScreen.tsx
+++ src/DevelopingScreen.tsx
@@ -4,7 +4,9 @@ import * as vid from './developing/vid.gif';
 
 export const DevelopingScreen = () => {
     return (
-        <div className="animated fadeIn" style={{height: '100%'}}>
-            <img src={vid} />
-        </div>);
+        <a href="https://github.com/mlnarik/thoughts" target="_blank">
+            <div className="animated fadeIn" style={{height: '100%'}}>
+                <img src={vid} style={{height: '100%'}}/>
+            </div>
+        </a>);
 }
